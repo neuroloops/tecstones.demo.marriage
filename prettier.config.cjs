c3152f3d22a6ed8e9c524c4bd429acce141322b4
Ludovic Dumas <ludoloops@pm.me>
2023-06-30 19:50:47 -0400

    better organized

---
+++ prettier.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  "semi": false
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xxs: ['8px', '11px']
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
